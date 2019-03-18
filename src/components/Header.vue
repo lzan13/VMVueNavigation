dacaf0c0017b8fca9249617b5a7e46ef002ce2e2
liulizheng <liulizheng@weidian.com>
2019-03-18 18:06:38 +0800

    添加了搜索工具选择框,可以选择多种不同工具

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,16 +11,24 @@
                 <a href="https://www.melove.net" target="_blank">关于我</a>
             </div>
             <div class="header-search">
+
+                <!-- <span>Selected: {{ selectEngines }}</span> -->
                 <ul class="search-tab">
-                    <li>搜索引擎:</li>
-                    <li v-for="(item, index) in tabList" :class="{'tab-active':currIndex==index}" v-on:click="tabChange(index)">
-                        {{ item.name }}
+                    <li>
+                        <select v-model="selectEngines">
+                            <option v-for="category in categorys" v-bind:value="category.engines">
+                                {{ category.title }}
+                            </option>
+                        </select>
+                    </li>
+                    <li v-for="(engine, index) in selectEngines" :class="{'tab-active':selectIndex==index}" v-on:click="tabChange(index)">
+                        {{ engine.name }}
                     </li>
                     <!-- <li v-for="(ind, key, index) in tabs" v-bind:class="{tab-active:(currIndex==index)}" v-on:click="changeTab(index)">{{ind}}</li> -->
                 </ul>
                 <div class="search-input">
-                    <div v-for="(item, index) in formList" class="search-form" v-if="currIndex==index">
-                        <input type="text" :placeholder="item.hint" v-model="searchContent" onfocus="this.select()" onmouseover="this.focus()">
+                    <div v-for="(engine, index) in selectEngines" class="search-form" v-if="selectIndex==index">
+                        <input type="text" :placeholder="engine.hint" v-model="searchContent" onfocus="this.select()" onmouseover="this.focus()">
                         <span v-on:click="search()">Enter/点我</span>
                     </div>
                 </div>
@@ -35,39 +43,86 @@ export default {
     name: "Header",
     data() {
         return {
-            engineList: [
+            categorys: [
                 {
                     title: "常用",
-                    tabs: [{ name: "搜狗" }, { name: "Google" }, { name: "百度" }, { name: "知乎" }, { name: "音乐" }],
                     engines: [
-                        { action: "https://www.sogou.com/web?query=", name: "query", hint: "无法翻墙用搜狗" },
-                        { action: "https://www.google.com/search?q=", name: "q", hint: "最强大的搜索引擎" },
-                        { action: "https://www.baidu.com/s?wd=", name: "wd", hint: "百度一下你就知道" },
-                        { action: "https://www.zhihu.com/search?q=", name: "q", hint: "有问题上知乎" },
-                        { action: "https://music.163.com/#/search/m/?s=", name: "s", hint: "网易云音乐" },
+                        { action: "https://www.sogou.com/web?query=", name: "搜狗", hint: "无法翻墙用搜狗" },
+                        { action: "https://www.google.com/search?q=", name: "Google", hint: "最强大的搜索引擎" },
+                        { action: "https://www.baidu.com/s?wd=", name: "百度", hint: "百度一下你就知道" },
+                        { action: "https://www.zhihu.com/search?q=", name: "知乎", hint: "有问题上知乎" },
+                        { action: "https://music.163.com/#/search/m/?s=", name: "云音乐", hint: "网易云音乐" },
                     ],
                 },
                 {
                     title: "搜索",
                     engines: [
-                        { action: "https://www.sogou.com/web?query=", name: "query", hint: "无法翻墙用搜狗" },
-                        { action: "https://www.google.com/search?q=", name: "q", hint: "最强大的搜索引擎" },
-                        { action: "https://www.baidu.com/s?wd=", name: "wd", hint: "百度一下你就知道" },
-                        { action: "https://cn.bing.com/search?q=", name: "q", hint: "Bing 搜索" },
-                        { action: "https://yz.m.sm.cn/s?q=", name: "q", hint: "神马搜索" },
+                        { action: "https://www.sogou.com/web?query=", name: "搜狗", hint: "无法翻墙用搜狗" },
+                        { action: "https://www.google.com/search?q=", name: "Google", hint: "最强大的搜索引擎" },
+                        { action: "https://cn.bing.com/search?q=", name: "Bing", hint: "喜欢微软就用Bing" },
+                        { action: "https://www.baidu.com/s?wd=", name: "百度", hint: "百度一下你就知道" },
+                        { action: "https://yz.m.sm.cn/s?q=", name: "神马", hint: "神马搜索" },
+                    ],
+                },
+                {
+                    title: "设计",
+                    engines: [
+                        { action: "https://www.csswinner.com/search/", name: "CSSWinner", hint: "输入快递单号查询" },
+                        { action: "https://www.google.com/search?q=", name: "Google", hint: "最强大的搜索引擎" },
+                        { action: "https://cn.bing.com/search?q=", name: "Bing", hint: "喜欢微软就用Bing" },
+                        { action: "https://www.baidu.com/s?wd=", name: "百度", hint: "百度一下你就知道" },
+                        { action: "https://yz.m.sm.cn/s?q=", name: "神马", hint: "神马搜索" },
+                    ],
+                },
+                {
+                    title: "影视",
+                    engines: [
+                        { action: "http://gaoqing.la/?s=", name: "高清", hint: "输入快递单号查询" },
+                        { action: "http://www.llduang.com/?s=", name: "大榴莲", hint: "最强大的搜索引擎" },
+                        { action: "https://cn.bing.com/search?q=", name: "Bing", hint: "喜欢微软就用Bing" },
+                        { action: "https://www.baidu.com/s?wd=", name: "百度", hint: "百度一下你就知道" },
+                        { action: "https://yz.m.sm.cn/s?q=", name: "神马", hint: "神马搜索" },
+                    ],
+                },
+                {
+                    title: "音乐",
+                    engines: [
+                        { action: "https://music.163.com/#/search/m/?s=", name: "云音乐", hint: "网易云音乐" },
+                        { action: "https://www.xiami.com/search?key=", name: "虾米", hint: "最强大的搜索引擎" },
+                        { action: "http://music.baidu.com/search?key=", name: "千千", hint: "喜欢微软就用Bing" },
+                        { action: "https://y.qq.com/portal/search.html#page=1&searchid=1&remoteplace=txt.yqq.top&t=song&w=", name: "QQ音乐", hint: "百度一下你就知道" },
+                        { action: "https://soundcloud.com/search?q=", name: "SoundCloud", hint: "神马搜索" },
+                        { action: "https://open.spotify.com/search/results/", name: "Spotify", hint: "神马搜索" },
+                    ],
+                },
+                {
+                    title: "工具",
+                    engines: [
+                        { action: "https://m.kuaidi100.com/result.jsp?nu=", name: "快递", hint: "输入快递单号查询" },
+                        { action: "https://www.google.com/search?q=", name: "Google", hint: "最强大的搜索引擎" },
+                        { action: "https://cn.bing.com/search?q=", name: "Bing", hint: "喜欢微软就用Bing" },
+                        { action: "https://www.baidu.com/s?wd=", name: "百度", hint: "百度一下你就知道" },
+                        { action: "https://yz.m.sm.cn/s?q=", name: "神马", hint: "神马搜索" },
                     ],
                 },
             ],
-            currIndex: 0,
+            selectEngines: [
+                { action: "https://www.sogou.com/web?query=", name: "搜狗", hint: "无法翻墙用搜狗" },
+                { action: "https://www.google.com/search?q=", name: "Google", hint: "最强大的搜索引擎" },
+                { action: "https://www.baidu.com/s?wd=", name: "百度", hint: "百度一下你就知道" },
+                { action: "https://www.zhihu.com/search?q=", name: "知乎", hint: "有问题上知乎" },
+                { action: "https://music.163.com/#/search/m/?s=", name: "云音乐", hint: "网易云音乐" },
+            ],
+            selectIndex: 0,
             searchContent: "",
         };
     },
     methods: {
         tabChange: function(index) {
-            this.currIndex = index;
+            this.selectIndex = index;
         },
         search: function() {
-            let url = this.formList[this.currIndex].action + this.searchContent;
+            let url = this.selectEngines[this.selectIndex].action + this.searchContent;
             window.open(url, "_blank");
         },
     },
@@ -144,11 +199,38 @@ export default {
                         color: @text-dark;
                     }
                     &:first-child {
+                        position: relative;
+                        width: 56px;
                         padding: 0 8px;
+                        text-align: center;
                         color: @text-light;
-                        cursor: auto;
                         &:hover {
-                            background-color: transparent;
+                            background-color: @bg-translucence-light;
+                        }
+                        &::after {
+                            content: "▼";
+                            position: absolute;
+                            top: 2px;
+                            right: 8px;
+                            width: 8px;
+                            height: 36px;
+                            line-height: 36px;
+                            font-size: 12px;
+                            text-align: center;
+                        }
+                        select {
+                            width: 100%;
+                            height: 36px;
+                            line-height: 36px;
+                            padding-left: 12px;
+                            //隐藏select的下拉图标
+                            appearance: none;
+                            -webkit-appearance: none;
+                            -moz-appearance: none;
+                            font-size: 16px;
+                            color: @text-light;
+                            background: @transparent;
+                            cursor: pointer;
                         }
                     }
                 }
